d0ac6c5c28fc4fca9122540ee808fba76f7104ab
Daniel Sasser <danielbsasser@gmail.com>
2020-03-19 15:27:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
     @word4 = params[:word4]
     @word5 = params[:word5]
 
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
   get '/:operation/:number1/:number2' do
@@ -45,7 +45,6 @@ class App < Sinatra::Base
     end
 
     @total.to_s
-
   end
 
 end
